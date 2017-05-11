17fa8dc082a5b859bbf68e2c240f2757c09878b8
Sitanun Changhor <dream.schanghor@gmail.com>
2017-05-10 22:44:18 -0300

    - Buttons/input now works

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,6 +2,10 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Reference: https://developer.android.com/reference/android/widget/Button.html
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                EditText edit = (EditText) findViewById(R.id.inputText);
+                TextView view = (TextView) findViewById(R.id.inputView);
+
+                view.setText(edit.getText().toString());
+            }
+        });
     }
 }
